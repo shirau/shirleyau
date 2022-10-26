0bef4d1573f0eb52e725cf3525bbb69340566623
Shirley Au <shirau@gmail.com>
2022-10-26 15:04:28 +0800

    Add files via upload

---
+++ konew/js.js
@@ -0,0 +1,26 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: '-1ChzCE3z2o',
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    player.setPlaybackRate(1.5);
+    event.target.playVideo();
+}
